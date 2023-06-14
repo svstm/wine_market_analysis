b8e198cbcdef2c204a330718ae7ed288cde86449
unknown <lozavteam@gmail.com>
2023-06-14 16:01:28 +0200

    commit_n

--- file.sql
+++ file.sql
@@ -2,9 +2,9 @@
 We want to highlight 10 wines to increase our sales, which ones should we choose and why?
 */
 
-SELECT NAME, (ratings_average / (bottle_volume_ml/price_euros)) AS Rate_per_price, ratings_average, ratings_count, price_euros
+SELECT NAME, ROUND((ratings_average / (bottle_volume_ml/price_euros)), 3) AS Rate_per_price, ratings_average, ratings_count, price_euros
 FROM vintages
-WHERE ratings_count > 100
+WHERE ratings_count > 1000
 ORDER BY (ratings_average / (bottle_volume_ml/price_euros)) ASC
 LIMIT 10
 
@@ -36,15 +36,72 @@ We has detected that a big cluster of customers like a specific combination of t
 	- citrus
 */
 
-SELECT wines.name
+SELECT wines.name,  keywords_wine.count, keywords_wine.group_name, wines.id
 FROM wines
 	JOIN keywords_wine ON  wines.id = keywords_wine.wine_id
 	JOIN keywords ON keywords.id = keywords_wine.keyword_id
 
 WHERE keywords.name IN ('coffee', 'toast', 'green apple', 'cream', 'citrus')	
-GROUP BY wines.name
-HAVING COUNT(DISTINCT keywords.name) = 5;
+AND keywords_wine.count > 10
+AND keywords_wine.keyword_type = 'primary'
+GROUP BY wines.name 
+HAVING COUNT(wines.id) = 5;
+
+
+
+
+-- SELECT wines.name,  keywords_wine.count, keywords_wine.group_name
+-- FROM wines
+-- 	JOIN keywords_wine ON  wines.id = keywords_wine.wine_id
+-- 	JOIN keywords ON keywords.id = keywords_wine.keyword_id
+
+-- WHERE keywords.name IN ('coffee', 'toast', 'green apple', 'cream', 'citrus')	
+-- AND keywords_wine.count > 10
+-- AND keywords_wine.keyword_type = 'primary'
+-- GROUP BY wines.name
+
 
 /*markdown
-
+ We would like to do a selection of wines that are easy to find all over the world. **Find the top 3 most common `grape` all over the world** and **for each grape, give us the the 5 best rated wines**.
 */
+
+SELECT DISTINCT(grapes.name) AS most_common_grape
+	FROM most_used_grapes_per_country
+	JOIN grapes
+	ON grapes.id = most_used_grapes_per_country.grape_id
+ORDER BY most_used_grapes_per_country.wines_count DESC
+LIMIT 3
+
+SELECT 
+FROM w
+
+/*markdown
+We would to give create a country leaderboard, give us a visual that shows the **average wine rating for each `country`**. Do the same for the `vintages
+*/
+
+SELECT countries.name, ROUND(AVG(wines.ratings_average), 2) AS average_rating
+FROM wines
+JOIN regions
+ON wines.region_id = regions.id
+JOIN countries
+ON regions.country_code = countries.code
+GROUP BY countries.name
+ORDER BY ROUND(AVG(wines.ratings_average), 2) DESC
+
+
+
+/*markdown
+Do the same for the `vintages
+*/
+
+SELECT countries.name, ROUND(AVG(vintages.ratings_average), 2) AS average_rating
+FROM wines
+JOIN vintages
+ON vintages.wine_id = wines.id
+JOIN regions
+ON wines.region_id = regions.id
+JOIN countries
+ON regions.country_code = countries.code
+GROUP BY countries.name
+ORDER BY average_rating DESC
+
